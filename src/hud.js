8be68104685213651c7117742a8affcd6453e156
KamiKid_ <95935025+nebula084@users.noreply.github.com>
2023-06-07 11:23:27 -0700

    Update hud.js

--- src/hud.js
+++ src/hud.js
@@ -331,7 +331,7 @@ export class Hud extends Phaser.Scene {
         if (that.dialogueLevel == 8) {
           that.dialogueLevel = 9;
         }
-      } else if (playerY >= 905) {
+      } else if (playerY >= 1100) {
         that.level = 2;
         that.levelText.setText('Level: Uterus')
 
